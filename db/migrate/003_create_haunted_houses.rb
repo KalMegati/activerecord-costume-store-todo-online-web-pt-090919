8d59042bae670cb56d7046232af564156644b118
Srikalyan Megati <kalmegati@gmail.com>
2019-11-18 20:28:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,9 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+    end
+  end
+end
